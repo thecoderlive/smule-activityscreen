bc98c5d00a78fb81c7f9021a157455ac6c26fc5d
The Coder <82015917+thecoderlive@users.noreply.github.com>
2022-12-07 17:34:04 +0530

    Initial Commit for file: DATA.js

---
+++ DATA.js
@@ -0,0 +1,70 @@
+export const item = {
+    "id": "aa7517bf-bf2a-4f63-aaa6-78a959ab21da",
+    "activity_list": [
+        {
+            "id": "96de3472-9196-4fa7-8931-89163f1e6e79",
+            "user_profile_img": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/42.png",
+            "user_name": "heavy",
+            "when_updated": "that",
+            "menu_icon": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/182.png",
+            "about_song": "Box article occur air hot energy. Adult pick hospital big yet other.",
+            "song_cover": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/6.png",
+            "song_name": "anything",
+            "singer_name": "all",
+            "song_views": "dream",
+            "group_icon": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/44.png"
+        },
+        {
+            "id": "b4b06dbf-7e91-496c-a9b9-3239c85f79ab",
+            "user_profile_img": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/80.png",
+            "user_name": "house",
+            "when_updated": "receive",
+            "menu_icon": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/132.png",
+            "about_song": "Family laugh music.",
+            "song_cover": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/308.png",
+            "song_name": "life",
+            "singer_name": "scientist",
+            "song_views": "suddenly",
+            "group_icon": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/59.png"
+        },
+        {
+            "id": "2dc95bc9-054b-4efb-8b76-9e3528459447",
+            "user_profile_img": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/167.png",
+            "user_name": "information",
+            "when_updated": "walk",
+            "menu_icon": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/20.png",
+            "about_song": "Follow they song get result. Pretty food find test lawyer structure store.",
+            "song_cover": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/220.png",
+            "song_name": "agency",
+            "singer_name": "medical",
+            "song_views": "positive",
+            "group_icon": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/281.png"
+        },
+        {
+            "id": "487fa04b-0f3c-4563-af59-80d292ba4b84",
+            "user_profile_img": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/302.png",
+            "user_name": "find",
+            "when_updated": "least",
+            "menu_icon": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/85.png",
+            "about_song": "Return leader first food soldier. Thing sell today fact order. Son past still somebody mission debate member.",
+            "song_cover": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/143.png",
+            "song_name": "choose",
+            "singer_name": "response",
+            "song_views": "child",
+            "group_icon": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/124.png"
+        },
+        {
+            "id": "178695c9-1ec0-4964-9126-65b874267af2",
+            "user_profile_img": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/159.png",
+            "user_name": "important",
+            "when_updated": "heavy",
+            "menu_icon": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/354.png",
+            "about_song": "Oil again modern short against.",
+            "song_cover": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/207.png",
+            "song_name": "support",
+            "singer_name": "nation",
+            "song_views": "she",
+            "group_icon": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/126.png"
+        }
+    ]
+}
